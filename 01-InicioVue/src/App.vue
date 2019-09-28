7732ce5972c87fa36e5108a3102044e9b0a0d0df
carloshquadrado <54450333+carloshquadrado@users.noreply.github.com>
2019-09-27 22:43:00 -0300

    texto secundário

--- 01-InicioVue/src/App.vue
+++ 01-InicioVue/src/App.vue
@@ -2,6 +2,8 @@
   <div>
     <div class="text-center">
       <h1>NameGator</h1>
+      <br/>
+      <h6 class="text-secondary">Gerador de Nomes utilizando Vue.js, GraphQL e Node</h6>
     </div>
   </div>
 </template>
